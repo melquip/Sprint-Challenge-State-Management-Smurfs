a975830acd0c2f38580b87ac69de58a2ba53ccc5
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-11 09:13:12 +0100

    Added form slice of state

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -11,8 +11,8 @@ import thunk from 'redux-thunk';
 const store = createStore(
 	combineReducers({
 		smurfs: reducers.smurfsReducer,
+		addSmurfForm: reducers.smurfFormReducer
 	}),
-	{},
 	compose(
 		applyMiddleware(thunk),
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
